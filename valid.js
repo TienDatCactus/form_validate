a7a44d4be24b503e337da1d15448c8ddbfa9759a
tien dat <multivncraft@gmail.com>
2024-04-17 19:28:34 +0700

    fixes some multiple rule process chaining

--- valid.js
+++ valid.js
@@ -1,39 +1,84 @@
 function validator(opts) {
-
-  var selectorRule = {};
-
+  var selectorRules = {};
   function validate(inputElement, rule) {
-    var error = rule.test(inputElement.value);
-    var message = inputElement.parentElement.querySelector(".form-msg");
-    if (error) {
-      message.innerText = error;
+    var error = inputElement.parentElement.querySelector(opts.errorSelector);
+    var message;
+
+    // takes out rules of selector
+    var ruled = selectorRules[rule.selector];
+    // loop through rules & check
+    // if error exists , stop
+    for (var i = 0; i < ruled.length; ++i) {
+      message = ruled[i](inputElement.value);
+      if (message) break;
+    }
+
+    if (message) {
+      error.innerText = message;
       inputElement.parentElement.classList.add("invalid");
     } else {
       inputElement.parentElement.classList.remove("invalid");
-      message.innerText = " ";
+      error.innerText = " ";
     }
+
+    return !message;
   }
   var form = document.querySelector(opts.form);
-  opts.rules.forEach(function (rule) {
-    // save rules for each input
-    selectorRule[rule.selector] = rule.test;
+  if (form) {
+    form.onsubmit = function (e) {
+      e.preventDefault();
 
-    var inputElement = form.querySelector(rule.selector);
-    var message = inputElement.parentElement.querySelector(opts.errorSelector);
-    if (inputElement) {
-      // blur out of input
-      inputElement.onblur = function () {
-        validate(inputElement, rule);
-      };
+      let isValid = true;
 
-      // while input
-      inputElement.oninput = function () {
-        inputElement.parentElement.classList.remove("invalid");
-        message.innerText = "";
-      };
-    }
-    console.log(selectorRule)
-  });
+      opts.rules.forEach(function (rule) {
+        var inputElement = form.querySelector(rule.selector);
+        var valid = validate(inputElement, rule);
+        if (!valid) {
+          isValid = false;
+        }
+      });
+
+      if (isValid) {
+        // submit with js
+        if (typeof opts.onSubmit === "function") {
+          var formData = form.querySelectorAll("[name]");
+          var formValue = Array.from(formData);
+          formValue.reduce(function (values, input) {
+            return (values[input.name] = input.value) && values;
+          }, {});
+          opts.onSubmit(formValue);
+        } // submit with default event
+        else {
+          form.submit();
+        }
+      }
+    };
+    // loop through and process ( events )
+    opts.rules.forEach(function (rule) {
+      // save rules for each input
+      if (Array.isArray(selectorRules[rule.selector])) {
+        selectorRules[rule.selector].push(rule.test);
+      } else {
+        selectorRules[rule.selector] = [rule.test];
+      }
+      var inputElement = form.querySelector(rule.selector);
+      var message = inputElement.parentElement.querySelector(
+        opts.errorSelector
+      );
+      if (inputElement) {
+        // blur out of input
+        inputElement.onblur = function () {
+          validate(inputElement, rule);
+        };
+
+        // while input
+        inputElement.oninput = function () {
+          inputElement.parentElement.classList.remove("invalid");
+          message.innerText = "";
+        };
+      }
+    });
+  }
 }
 
 // Định nghĩa rule
